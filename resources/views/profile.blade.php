af88508c5cc29d84714b3fe5a9bcc5af3eb7358e
[satoukatsuki] <[katsuki0607@gmail.com]>
2020-09-01 14:39:05 +0900

    initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>プロフィール</title>
+</head>
+<body>
+
+    <!-- ここにプロフィールを記入 -->
+    <h2>自己紹介</h5>
+    <p>名前:佐藤伽月</p>
+    <p>年齢:24</p>
+    <p>出身:埼玉県新座市</p>
+</body>
+</html>
